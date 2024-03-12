2889e8b3501071aed7db13ac8e4d79fd867a8ada
JulieFontes <jmfontes3@gmail.com>
2024-03-11 20:37:52 -0400

    Initial Commit

---
+++ App.LMS/Library.LMS/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Library.LMS.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Dictionary<int, double> Grades { get; set; }
+
+        public char Classification { get; set; }
+
+        public Person()
+        {
+            Name = string.Empty;
+            Grades = new Dictionary<int, double>();
+        }
+    }
+}
