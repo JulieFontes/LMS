eb971e688899f4c0cdea1003a8df411e403506db
JulieFontes <jmfontes3@gmail.com>
2024-03-19 18:34:42 -0400

    Initial Commit

--- App.LMS/App.LMS/Program.cs
+++ App.LMS/App.LMS/Program.cs
@@ -14,54 +14,81 @@ namespace CanvasApp
             bool keepGoing = true;
 
             while (keepGoing) {
-                Console.WriteLine("Menu:\nA. Add a student\nUS. Update Student \nB. List all enrolled students\nC. Search for a student" +
-                    "\nD. Add a course\nUC. Update a Course\nE. List all courses\nF. Search Courses\nEXIT/QUIT ");
+                Console.WriteLine("Do maintanence on:\nS. Student\nC. Courses?");
+                Console.WriteLine("EXIT/QUIT ");
                 string choice = Console.ReadLine()?.ToUpper() ?? string.Empty;
+                switch (choice) 
+                {
+                    case "S":
+                        ShowStudentMenu(studentHelper);
+                        break;
 
-                switch (choice) {
-                    case "A":
-                        studentHelper.CreateStudent();
-                        break;
-                    case "US":
-                        studentHelper.UpdateStudent();
-                        break;
-                    
-                    case "B":
-                        studentHelper.ListStudents();
-                        break;
-                    
                     case "C":
-                        studentHelper.SearchStudents();
+                        ShowCourseMenu(courseHelper);
                         break;
 
-                    case "D":
-                        courseHelper.CreateCourse();
-                        break;
-
-                    case "UC":
-                        courseHelper.UpdateCourse();
-                        break;
-
-                    case "E":
-                        courseHelper.ListCourses();
-                        break;
-
-                    case "F":
-                        courseHelper.SearchCourses();
-                        break;
-                    
                     case "EXIT":
                     case "QUIT":
                         keepGoing = false;
                         break;
 
                     default:
-                        Console.WriteLine("\nInvalid menu option.\n");
+                        Console.WriteLine("\nInvalid option.\n");
                         break;                     
                 }
 
             }
 
+        }
+
+        static void ShowStudentMenu(StudentConsoleHelper studentHelper) 
+        {
+            Console.WriteLine("Select an action:\n " + "A. Add a student\nUS. Update Student \nB. List all enrolled students\nC. Search for a student");
+            string choice = Console.ReadLine() ?? string.Empty;
+            
+            switch (choice.ToUpper()) 
+            {
+                case "A":
+                    studentHelper.CreateStudent();
+                    break;
+                case "US":
+                    studentHelper.UpdateStudent();
+                    break;
+
+                case "B":
+                    studentHelper.ListStudents();
+                    break;
+
+                case "C":
+                    studentHelper.SearchStudents();
+                    break;
+
+            }
+        }
+
+        static void ShowCourseMenu(CourseConsoleHelper courseHelper)
+        {
+            Console.WriteLine("Select an action:\n" + "D.Add a course\nUC.Update a Course\nE.List all courses\nF.Search Courses\n");
+            string choice = Console.ReadLine() ?? string.Empty;
+
+            switch (choice.ToUpper())
+            {
+                case "D":
+                    courseHelper.CreateCourse();
+                    break;
+
+                case "UC":
+                    courseHelper.UpdateCourse();
+                    break;
+
+                case "E":
+                    courseHelper.ListCourses();
+                    break;
+
+                case "F":
+                    courseHelper.SearchCourses();
+                    break;
+            }
         }
     }
 }
--- App.LMS/Library.LMS/Models/ContentItem.cs
+++ App.LMS/Library.LMS/Models/ContentItem.cs
@@ -8,12 +8,10 @@ namespace Library.LMS.Models
 {
     public class ContentItem
     {
-
+        public int Id { get; set; }
         public string Name { get; set; }
 
-        public string Description { get; set; }
-
-        public string? Path { get; set; }
+        public string? Description { get; set; }
 
         public ContentItem() {
             Name = string.Empty;
